a30ff437b07549d0e3af02e3b228ca83fea8204b
Kelello-7206 <tseripe7@gmail.com>
2023-07-31 15:14:25 +0200

    delete previewBar component

--- Podcast/src/App.jsx
+++ Podcast/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import './index.css'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
@@ -7,29 +7,11 @@ import PreviewBar from "./components/PreviewBar"
 
 function App() {
 
-  const [showPodcast, setPodcast] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
-
-  useEffect(() => {
-    // Fetch all show data from the API
-    fetch('https://podcast-api.netlify.app/shows')
-      .then((response) => response.json())
-      .then((data) => {
-        setPodcast(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching show data:', error);
-        setLoading(false);
-      });
-  }, []);
-
   return (
     <>
       <Navbar />
-      <PreviewBar shows={showPodcast} loading={loading} />
-
-      <Home shows={showPodcast} loading={loading} />
+ 
+      <Home />
     </>
   )
 }
