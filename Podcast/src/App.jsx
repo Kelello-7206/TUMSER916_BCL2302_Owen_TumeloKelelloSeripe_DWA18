321796864b8ed6461df55c6cc38d94de1e4a1f7d
Kelello-7206 <tseripe7@gmail.com>
2023-07-31 04:49:21 +0200

    added react-router-dom

--- Podcast/src/App.jsx
+++ Podcast/src/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import './index.css'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 
 function App() {
 
+  useEffect(() => {
+    // Fetch all show data from the API
+    fetch('https://podcast-api.netlify.app/shows')
+      .then((response) => response.json())
+      .then((data) => {
+        setShows(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching show data:', error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <>
       <Navbar />
-   
+      <Home shows={shows} loading={loading} />
     </>
   )
 }
--- Podcast/src/components/Home.jsx
+++ Podcast/src/components/Home.jsx
@@ -1,28 +1,31 @@
 import React from "react"
-import { Link } from 'react-router-dom';
-import '../index.css'
+import Link from 'react-router-dom'
 
 
-export default function Home() {
-
-    useEffect(() => {
-        // Fetch all show data from the API
-        fetch('https://podcast-api.netlify.app/shows')
-          .then((response) => response.json())
-          .then((data) => {
-            setShows(data);
-            setLoading(false);
-          })
-          .catch((error) => {
-            console.error('Error fetching show data:', error);
-            setLoading(false);
-          });
-      }, []);
-      
+export default function Home({ shows, loading }) {
   return (
- 
+    <div className="home-container">
+      <h1>All Shows</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="show-list">
+          {shows.map((show) => (
+            <li key={show.id}>
+              <Link to={`/show/${show.id}`} className="show-link">
+                <div className="show-info">
+                  <img src={show.image} alt={show.title} className="show-image" />
+                  <div className="show-details">
+                    <h3 className="show-title">{show.title}</h3>
+                    <p className="show-description">{show.description}</p>
+                  </div>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
-
-
 }
 
