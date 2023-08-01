55a7b2064d9a12a04b91f8dad13039322faae209
Kelello-7206 <tseripe7@gmail.com>
2023-08-01 13:30:07 +0200

    deleted Home component css

--- Podcast/src/components/Home.jsx
+++ Podcast/src/components/Home.jsx
@@ -1,69 +1,69 @@
-// import React, { useEffect } from "react";
-// import axios from "axios";
+import React, { useEffect } from "react";
+import axios from "axios";
 
-// export default function Home() {
-//   const [showPodcast, setPodcast] = React.useState([]);
-//   const [loading, setLoading] = React.useState(true);
+export default function Home() {
+  const [showPodcast, setPodcast] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
-//   useEffect(() => {
+  useEffect(() => {
    
-//     axios
-//       .get('https://podcast-api.netlify.app/shows')
-//       .then((response) => {
-//         setPodcast(response.data);
-//         setLoading(false);
-//       })
-//       .catch((error) => {
-//         console.error('Error fetching show data:', error);
-//         setLoading(false);
-//       });
-//   }, []);
+    axios
+      .get('https://podcast-api.netlify.app/shows')
+      .then((response) => {
+        setPodcast(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching show data:', error);
+        setLoading(false);
+      });
+  }, []);
 
-//   const genreData = {
-//     1: 'Personal Growth',
-//     2: 'True Crime and Investigative Journalism',
-//     3: 'History',
-//     4: 'Comedy',
-//     5: 'Entertainment',
-//     6: 'Business',
-//     7: 'Fiction',
-//     8: 'News',
-//     9: 'Kids and Family',
-//   };
+  const genreData = {
+    1: 'Personal Growth',
+    2: 'True Crime and Investigative Journalism',
+    3: 'History',
+    4: 'Comedy',
+    5: 'Entertainment',
+    6: 'Business',
+    7: 'Fiction',
+    8: 'News',
+    9: 'Kids and Family',
+  };
 
-//   const getGenres = (genreIds) => {
-//     if (Array.isArray(genreIds)) {
-//       return genreIds.map((id) => genreData[id]).join(', ');
-//     } else {
-//       return genreData[genreIds];
-//     }
-//   };
+  const getGenres = (genreIds) => {
+    if (Array.isArray(genreIds)) {
+      return genreIds.map((id) => genreData[id]).join(', ');
+    } else {
+      return genreData[genreIds];
+    }
+  };
 
-//   return (
-//     <div className="home-container">
-//       <h1>All Shows</h1>
-//       {loading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         <ul className="show-list">
-//           {showPodcast.map((show) => (
-//             <li key={show.id}>
-//               <a href={`/show/${show.id}`} className="show-link">
-//                 <div className="show-info">
-//                   <img src={show.image} className="show-image" alt={show.title} />
-//                   <div className="show-details">
-//                     <h3 className="show-title">{show.title}</h3>
-//                     <p className="show-description">{show.description}</p>
-//                     <p>Genre: {getGenres(show.genres)}</p>
-//                     <p className="show-seasons">Numbers of seasons: {show.seasons}</p>
-//                     <p className="show-updated">Updated: {show.updated}</p>
-//                   </div>
-//                 </div>
-//               </a>
-//             </li>
-//           ))}
-//         </ul>
-//       )}
-//     </div>
-//   );
-// }
+  return (
+    <div className="home-container">
+      <h1>All Shows</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="show-list">
+          {showPodcast.map((show) => (
+            <li key={show.id}>
+              <a href={`/show/${show.id}`} className="show-link">
+                <div className="show-info">
+                  <img src={show.image} className="show-image" alt={show.title} />
+                  <div className="show-details">
+                    <h3 className="show-title">{show.title}</h3>
+                    <p className="show-description">{show.description}</p>
+                    <p>Genre: {getGenres(show.genres)}</p>
+                    <p className="show-seasons">Numbers of seasons: {show.seasons}</p>
+                    <p className="show-updated">Updated: {show.updated}</p>
+                  </div>
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
