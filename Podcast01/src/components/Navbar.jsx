99f35612a543b88e6a99b94a78214dad24e351b2
Kelello-7206 <tseripe7@gmail.com>
2023-08-15 11:30:25 +0200

    commit default

--- Podcast01/src/components/Navbar.jsx
+++ Podcast01/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { AppBar, Toolbar, Button, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { AppBar, Toolbar, Button, Typography, createTheme, ThemeProvider } from '@mui/material';
+import { makeStyles } from '';
+
+const theme = createTheme();
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,20 +23,22 @@ function Navbar() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <div className={classes.buttonContainer}>
-            <Button color="inherit">Home</Button>
-            <Button color="inherit">Favorite</Button>
-            <Button color="inherit">History</Button>
-          </div>
-          <Typography variant="h6" className={classes.title}>
-            Navbar Title
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <div className={classes.buttonContainer}>
+              <Button color="inherit">Home</Button>
+              <Button color="inherit">Favorite</Button>
+              <Button color="inherit">History</Button>
+            </div>
+            <Typography variant="h6" className={classes.title}>
+              Navbar Title
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </ThemeProvider>
   );
 }
 
