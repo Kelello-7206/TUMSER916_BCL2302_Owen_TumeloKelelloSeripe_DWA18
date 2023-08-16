dc2381a6cbc4541ac93bf501f64d775b08d72e32
Kelello-7206 <tseripe7@gmail.com>
2023-08-16 16:17:04 +0200

    added MUI to navbar component

--- Podcast01/src/components/Navbar.jsx
+++ Podcast01/src/components/Navbar.jsx
@@ -3,23 +3,20 @@ import { AppBar, Toolbar, Button, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles((theme) => ({
-  appBar: {
-    backgroundColor: 'red',
+  navbar: {
+    backgroundColor: 'black',
   },
-  title: {
+  navTitle: {
     flexGrow: 1,
   },
   buttonGroup: {
     display: 'flex',
     alignItems: 'center',
-  },
-  greenButton: {
     color: 'white',
-    backgroundColor: 'green',
-    border: '1px solid white',
-    marginLeft: theme.spacing(2),
-    '&:hover': {
-      backgroundColor: 'darkgreen',
+    '& > button': {
+      backgroundColor: 'green',
+      border: '1px solid blue',
+      marginLeft: theme.spacing(2),
     },
   },
 }));
@@ -28,15 +25,15 @@ const Navbar = () => {
   const classes = useStyles();
 
   return (
-    <AppBar position="static" >
-      <Toolbar className={classes.appBar}>
-        <Typography variant="h5" className={classes.title}>
+    <AppBar position="static">
+      <Toolbar className={classes.navbar}>
+        <Typography variant="h5" className={classes.navTitle}>
           Your Logo
         </Typography>
         <div className={classes.buttonGroup}>
-          <Button className={classes.greenButton}>Home</Button>
-          <Button className={classes.greenButton}>Favorite</Button>
-          <Button className={classes.greenButton}>History</Button>
+          <Button>Home</Button>
+          <Button>Favorite</Button>
+          <Button>History</Button>
         </div>
       </Toolbar>
     </AppBar>
