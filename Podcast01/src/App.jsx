9fca53a7a78df4a03e71627808e5454b67af948a
Kelello-7206 <tseripe7@gmail.com>
2023-08-18 11:01:08 +0200

    Deleted pages folder

--- Podcast01/src/App.jsx
+++ Podcast01/src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 const theme = createTheme();
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <div className="App">
-        {/* Your other components */}
         <Navbar />
+        
+        <Home />
       </div>
     </ThemeProvider>
   );
--- Podcast01/src/components/Home.jsx
+++ Podcast01/src/components/Home.jsx
@@ -1,24 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Homepage from '../pages/homepage';
+import { Card, CardContent, CardMedia, Typography, Container, Grid } from '@mui/material';
 
 const Home = () => {
   const [shows, setShows] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchShows = async () => {
       try {
         const response = await axios.get('https://podcast-api.netlify.app/shows');
         setShows(response.data);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching shows:', error);
+        setLoading(false);
       }
     };
 
     fetchShows();
   }, []);
 
-  return <Homepage shows={shows} />;
+  return (
+    <>
+      <Container>
+        <Grid container spacing={2}>
+          {shows.map((show) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={show.id}>
+              <Card>
+                <CardMedia component="img" height="140" image={show.previewImage} alt={show.name} />
+                <CardContent>
+                  <Typography variant="h6">{show.name}</Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {`Seasons: ${show.seasons}`}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {`Last Updated: ${new Date(show.lastUpdated).toDateString()}`}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {`Genres: ${show.genres.join(', ')}`}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
