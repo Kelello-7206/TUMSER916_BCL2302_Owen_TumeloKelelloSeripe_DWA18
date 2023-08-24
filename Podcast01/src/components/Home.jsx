4296843eaa6aa047b2deabb0972965d4ddefa3bb
Kelello-7206 <tseripe7@gmail.com>
2023-08-24 21:37:49 +0200

    modified component to display genres

--- Podcast01/src/components/Home.jsx
+++ Podcast01/src/components/Home.jsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar';
 
 const Home = () => {
   const [shows, setShows] = useState([]);
-  const [loading, setLoading] = useState(true); // Declare the loading state
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchShows = async () => {
@@ -22,9 +22,32 @@ const Home = () => {
     fetchShows();
   }, []);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  };
+  
+  const genreData = {
+    1: 'Personal Growth',
+    2: 'True Crime and Investigative Journalism',
+    3: 'History',
+    4: 'Comedy',
+    5: 'Entertainment',
+    6: 'Business',
+    7: 'Fiction',
+    8: 'News',
+    9: 'Kids and Family',
+  };
+
+  const getGenres = (genreIds) => {
+    if (Array.isArray(genreIds)) {
+      return genreIds.map((id) => genreData[id]).join(', ');
+    } else {
+      return genreData[genreIds];
+    }
+  };
 
   return (
     <>
@@ -41,16 +64,16 @@ const Home = () => {
                     {show.title}
                   </Typography>
                   <Typography variant="body2" color="textSecondary">
-                  <span style={{ fontWeight: 'bold' }}>Description:</span> {show.description}
+                    <span style={{ fontWeight: 'bold' }}>Description:</span> {show.description}
                   </Typography>
                   <Typography variant="body2" color="textSecondary">
-                  <span style={{ fontWeight: 'bold' }}>Seasons:</span> {show.seasons}
+                    <span style={{ fontWeight: 'bold' }}>Seasons:</span> {show.seasons}
                   </Typography>
                   <Typography variant="body2" color="textSecondary">
-                  <span style={{ fontWeight: 'bold' }}>Last Updated:</span> {new Date(show.lastUpdated).toDateString()}
+                    <span style={{ fontWeight: 'bold' }}>Last Updated:</span> {formatDate(show.updated)}
                   </Typography>
                   <Typography variant="body2" color="textSecondary">
-                    <span style={{ fontWeight: 'bold' }}>Genres:</span> {show.genres.join(', ')}
+                    <span style={{ fontWeight: 'bold' }}>Genres:</span> {getGenres(show.genres)}
                   </Typography>
                 </CardContent>
               </Card>
