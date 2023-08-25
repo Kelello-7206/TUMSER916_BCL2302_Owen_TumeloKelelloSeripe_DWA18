fd6122bfe4f115b6c38cc3f289834b91c1f5a42d
Kelello-7206 <tseripe7@gmail.com>
2023-08-25 23:18:54 +0200

    add inline style to the div (app component)

--- Podcast01/src/App.jsx
+++ Podcast01/src/App.jsx
@@ -3,16 +3,30 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Home from './components/Home';
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#00bcd4',
+    },
+    background: {
+      default: '#222',
+      paper: '#333',
+    },
+  },
+  typography: {
+    fontFamily: 'Arial, sans-serif',
+  },
+});
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <div className="App">
+      <CssBaseline />
+      <div className="App" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         <Home />
-    </div>
-  </ThemeProvider>
+      </div>
+    </ThemeProvider>
   );
 }
 
